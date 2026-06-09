66b6dae98df4f777f75d2968993640ba12b77ed4
dingdong的mac <xopaodot@163.com>
2026-06-09 22:25:11 +0800

    增加yaml脚本

--- NMKSniper/text/NMKSReliabilityUndertakeController.swift
+++ NMKSniper/text/NMKSReliabilityUndertakeController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+/*
+ 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
+ */
+
 
 class NMKSReliabilityUndertakeController: UIViewController, MWHPetrollendingDelegate,MWHSteadyRemoteDelegate {
   
